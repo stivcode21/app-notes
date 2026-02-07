a62ac9243786bd2698449875d35e6311f428dcc5
stivcode21 <stivenelcodificador@gmail.com>
2026-02-07 17:47:26 -0500

    feat: init setup project

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Notas from "./screens/Notas";
+import CreateNote from "./screens/CreateNote";
+import DetailsNote from "./screens/DetailsNote";
 
 export default function App() {
+  const Stack = createStackNavigator();
+
+  function MyStack() {
+    return (
+      <Stack.Navigator
+        screenOptions={{
+          headerTitleAlign: "center",
+          headerStyle: { backgroundColor: "#111827" },
+          headerTintColor: "#f8fafc",
+          headerTitleStyle: { fontSize: 16, fontWeight: "700" },
+        }}
+      >
+        <Stack.Screen
+          name="Notas"
+          component={Notas}
+          options={{
+            title: "NOTAS",
+          }}
+        />
+        <Stack.Screen
+          name="Crear"
+          component={CreateNote}
+          options={{
+            title: "CREAR NOTAS",
+          }}
+        />
+        <Stack.Screen
+          name="Detail"
+          component={DetailsNote}
+          options={{
+            title: "DETALLES NOTAS",
+          }}
+        />
+      </Stack.Navigator>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
